0e16dbc7ab792063121d2fc9cbfaeb7f19d8e803
chenaoh <jjmontealto@uamv.edu.ni>
2026-05-26 12:33:43 -0600

    Modulo 3 y 4 estas listos

--- src/main/java/org/example/ejemplo/modelo/Cliente.java
+++ src/main/java/org/example/ejemplo/modelo/Cliente.java
@@ -3,12 +3,17 @@ package org.example.ejemplo.modelo;
 import lombok.Getter;
 import lombok.Setter;
 import org.openxava.annotations.Required;
+import org.openxava.annotations.View;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@View(
+        name = "Simple",
+        members = "numero, nombre"
+)
 public class Cliente {
 
     @Id
